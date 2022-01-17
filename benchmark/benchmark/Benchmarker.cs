c2089c5d14f96de3d1429fb4c603a5bc49f2fbc3
Gabriel Faria <gabriel@goncalvesfaria.com>
2022-01-16 21:08:35 -0300

    finished studying this really cool stuff

--- benchmark/benchmark/Benchmarker.cs
+++ benchmark/benchmark/Benchmarker.cs
@@ -11,10 +11,34 @@ namespace benchmark
         private const string DateTime = "2019-12-13T16:33:06Z";
         private static readonly Benchmarkable benchmarkable = new Benchmarkable();
 
-        [Benchmark]
+        [Benchmark(Baseline = true)]
         public void GetYearFromDateTime()
         {
             benchmarkable.GetYearFromDateTime(DateTime); 
+        }
+
+        [Benchmark]
+        public void GetYearFromSplit()
+        {
+            benchmarkable.GetYearFromSplit(DateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSubstring()
+        {
+            benchmarkable.GetYearFromSubstring(DateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSpan()
+        {
+            benchmarkable.GetYearFromSpan(DateTime);
+        }        
+        
+        [Benchmark]
+        public void GetYearFromSpanWithManualConversion()
+        {
+            benchmarkable.GetYearFromSpanWithManualConversion(DateTime);
         }
     }
 }
